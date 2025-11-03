bcf647a40bb9a19b96a20adadf8352656a272c96
Nico Smith <web@niiccoo2.xyz>
2025-11-02 22:10:15 -0500

    Inital Commit

---
+++ main.asm
@@ -0,0 +1,38 @@
+; assemble & link (PowerShell):
+; nasm -f win64 main.asm -o main.obj
+; gcc -nostdlib "-Wl,-e,_start" main.obj -o main.exe -lkernel32
+; .\hello.exe
+; echo $LASTEXITCODE   <-- shows the result (y) in PowerShell
+
+default rel
+extern ExitProcess
+global _start
+
+section .text
+_start:
+    ; your original idea: push args, call function
+    push 2         ; slope
+    push 4         ; y-intercept
+    push 5         ; x
+    call find_y    ; RAX <- y
+
+    ; ExitProcess(y)
+    sub  rsp, 40   ; Windows x64: 32B shadow space + keep 16B alignment
+    mov  ecx, eax  ; exit code in ECX
+    call ExitProcess
+    ; (no return)
+
+; y = slope * x + intercept
+find_y:
+    ; After CALL, stack top = return address, then x, intercept, slope.
+    pop  r11       ; save return address
+    pop  r8        ; x
+    pop  r9        ; intercept
+    pop  r10       ; slope
+
+    imul r8, r10   ; r8 = x * slope
+    add  r8, r9    ; r8 += intercept
+    mov  rax, r8   ; return y in RAX
+
+    push r11       ; restore return address
+    ret
