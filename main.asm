261d17bbdad19459e1f5a688272ac8d1048c1947
Nico Smith <web@niiccoo2.xyz>
2025-11-03 07:43:19 -0500

    Add stdout

--- main.asm
+++ main.asm
@@ -1,49 +1,71 @@
 ; assemble & link (PowerShell):
 ; nasm -f win64 main.asm -o main.obj
-; gcc -nostdlib "-Wl,-e,_start" main.obj -o main.exe -lkernel32
+; gcc -nostdlib "-Wl,-e,main" main.obj -o main.exe -lkernel32
 ; .\hello.exe
 ; echo $LASTEXITCODE   <-- shows the result (y) in PowerShell
 
 default rel
 extern ExitProcess
-global _start
+global main
 
-section .data
-    ; msg is a label
-    ; db = Data Bytes
-    ; saves the ASCII number equivalent of this msg into memory, retrievable later by its label
-    ; 10 is ASCII for a newline
-    msg: db "Hello, world!", 10
 
-    ; Define an assemble-time constant, which is calculated during compilation
-    ; Calculate len = string length.  subtract the address of the start of the string from the current position ($)
-    .len: equ $ - msg
+section .data ; init data here
+    num db 0           ; for storing ASCII char
+    newline db 13,10
+
+section .bss ; reserve space here
+    bytes_written resd 1
 
 section .text
-_start:
-    ; push args, call function
-    push 2         ; slope
-    push 4         ; y-intercept
-    push 5         ; x
-    call find_y    ; RAX <- y
+    global main
+    extern GetStdHandle, WriteFile, ExitProcess
+    STD_OUTPUT_HANDLE equ -11
 
-    ; ExitProcess(y)
-    sub  rsp, 40   ; Windows x64: 32B shadow space + keep 16B alignment
-    mov  ecx, eax  ; exit code in ECX
+main:
+    sub rsp, 32
+
+    mov rax, 0 ; init rax as 0 for debug
+
+    push 2 ; slope
+    push 3 ; intercept
+    push 2 ; x
+    call find_y ; should send output to rax
+
+    ; Convert integer 7 -> ASCII '7'
+    add al, '0'
+    mov [num], al
+
+    ; Get handle to stdout
+    mov ecx, STD_OUTPUT_HANDLE
+    call GetStdHandle
+
+    ; WriteFile(hOut, num, 1, &bytes_written, NULL)
+    mov rcx, rax
+    lea rdx, [rel num]
+    mov r8d, 1
+    lea r9, [rel bytes_written]
+    mov qword [rsp+20h], 0
+    call WriteFile
+
+    ; Write newline
+    lea rdx, [rel newline]
+    mov r8d, 2
+    call WriteFile
+
+    xor ecx, ecx
     call ExitProcess
-    ; (no return)
 
 ; y = slope * x + intercept
 find_y:
     ; After CALL, stack top = return address, then x, intercept, slope.
-    pop  r11       ; save return address
-    pop  r8        ; x
-    pop  r9        ; intercept
-    pop  r10       ; slope
+    pop  r13       ; save return address
+    pop  r10        ; x
+    pop  r11        ; intercept
+    pop  r12       ; slope
 
-    imul r8, r10   ; r8 = x * slope
-    add  r8, r9    ; r8 += intercept
-    mov  rax, r8   ; return y in RAX
+    imul r10, r12   ; r10 = x * slope
+    add  r10, r11    ; r10 += intercept
+    mov  rax, r10   ; return y in RAX
 
-    push r11       ; restore return address
+    push r13       ; restore return address
     ret
